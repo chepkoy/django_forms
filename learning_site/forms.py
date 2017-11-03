252f90f417452ec2f8b1cbcb57b98de636c6abe8
chepkoy <allankiplangat22@gmail.com>
2017-11-03 23:01:22 +0300

    Custom Field Validation

--- learning_site/forms.py
+++ learning_site/forms.py
@@ -4,4 +4,15 @@ class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               label="Leave Empty")
+
+    def clean_honeypot(self):
+        honeypot = self.cleaned_data['honeypot']
+        if len(honeypot):
+            raise forms.ValidationError(
+                " Honeypot should be left empty. Bad bot")
+        return honeypot
+
 
